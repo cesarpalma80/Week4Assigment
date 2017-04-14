c1a81c52c4b083f9f242bacb8d6c3bce4daf4f4c
Cesar <cesarpalma@macbook-air.hitronhub.home>
2017-04-13 22:45:57 -0600

    Initial Commit

---
+++ BikeRide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BikeRide
+//
+//  Created by Cesar on 13/04/17.
+//  Copyright © 2017 Metalbytes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
